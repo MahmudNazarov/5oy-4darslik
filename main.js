9155a3799c7045e52f31dfd1ab1fde115959eda8
MahmudNazarov <sh.nazarov@gmail.com>
2024-12-11 19:39:07 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const name = prompt("ismingizni kirting:")
 const letter = prompt("bita harf kirting:");
-if( name ||letter === null){
+if(name , letter == "") {
  alert("harif krit san chort")
 }else if(name.toLowerCase().includes(letter.toLowerCase())) {
   alert(`"${name}" isimda "${letter}" harifi bor.`);
-} else {
+}else {
   alert(`"${name}" isimda "${letter}" harifi yoq.`);
 }
